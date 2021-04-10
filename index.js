39cd84d22d56c7fe155f4db919d1258046c4c74e
ghost3131 <ghost5742@protonmail.com>
2021-04-10 20:10:27 +0300

    db

---
+++ index.js
@@ -0,0 +1,119 @@
+const fs = require('fs')
+
+class DB {
+
+    constructor(){
+
+    }
+
+    /*
+    yaz => set
+    db.set('prefix', '!')
+    <DB>.yaz('prefix', '!')
+    */
+
+    yaz(veri, değer){
+        if (!veri) throw new TypeError("Veri Girmediniz.")
+        if (!değer) throw new TypeError("Değer Girmediniz.")
+        const dosya = JSON.parse(fs.readFileSync('database.json', 'utf8'))
+        dosya[veri] = değer
+        return fs.writeFileSync('database.json', JSON.stringify(dosya, null, 2))
+    }
+
+    /*
+    bul => fetch/get
+    db.fetch('prefix')
+    <DB>.bul('prefix')
+    */
+
+    bul(veri){
+        if (!veri) throw new TypeError("Veri Girmediniz.")
+        const dosya = JSON.parse(fs.readFileSync('database.json', 'utf8'))
+        if (!dosya[veri]) throw new TypeError('YAZDIĞINIZ VERİ BULUNAMADI')
+        return dosya[veri]
+    }
+
+    /*
+    kontrol => has 
+    db.has('prefix')
+    <DB>.kontrol('prefix')
+    */
+
+    kontrol(veri){
+        if (!veri) throw new TypeError("Veri Girmediniz.")
+        const dosya = JSON.parse(fs.readFileSync('database.json', 'utf8'))
+        return dosya[veri] ? true : false 
+    }
+
+    /*
+    sil => delete
+    db.delete('prefix')
+    <DB>.sil('prefix')
+    */
+
+    sil(veri){
+        if (!veri) throw new TypeError("Veri Girmediniz.")
+        const dosya = JSON.parse(fs.readFileSync('database.json', 'utf8'))
+        if (!dosya[veri]) throw new TypeError('YAZDIĞINIZ VERİ BULUNAMADI')
+        delete dosya[veri]
+        return fs.writeFileSync('database.json', JSON.stringify(dosya, null , 2))
+    }
+
+    /*
+    yedekle => backup
+    db.backup('veri.json')
+    <DB>.yedekle('veri')
+    */
+
+    yedekle(dosyaAdı){
+        if (!dosyaAdı) throw new TypeError("Dosya Adı Girmediniz.")
+        const dosya = JSON.parse(fs.readFileSync('database.json', 'utf8'))
+        return fs.writeFileSync(`${dosyaAdı}.json`, JSON.stringify(dosya, null, 2))
+    }
+
+    /*
+    topla => add
+    db.add('puan', 5)
+    <DB>.topla('puan', 5)
+    */
+
+    topla(veri, değer){
+        if (!veri) throw new TypeError("Veri Girmediniz.")
+        if (typeof değer !== 'number') throw new TypeError("Değer için bir sayı giriniz.")
+        if (!this.kontrol(veri)) throw new TypeError("Veri olarak girdiğiniz değer veritabanında bulunamadı")
+        if (typeof this.bul(veri) !== 'number') throw new TypeError('Sayı ekleyeceğiniz değer sayı olmalıdır.')
+        const dosya = JSON.parse(fs.readFileSync('database.json', 'utf8'))
+        dosya[veri] += değer
+        return fs.writeFileSync('database.json', JSON.stringify(dosya, null, 2))
+    }
+
+    /*
+    çıkar => substr
+    db.substr('puan', 5)
+    <DB>.çıkar('puan', 5) 
+    */
+
+    çıkar(veri, değer) {
+        if (!veri) throw new TypeError("Veri Girmediniz.")
+        if (typeof değer !== 'number') throw new TypeError("Değer için bir sayı giriniz.")
+        if (!this.kontrol(veri)) throw new TypeError("Veri olarak girdiğiniz değer veritabanında bulunamadı")
+        if (typeof this.bul(veri) !== 'number') throw new TypeError('Sayı ekleyeceğiniz değer sayı olmalıdır.')
+        const dosya = JSON.parse(fs.readFileSync('database.json', 'utf8'))
+        dosya[veri] -= değer
+        return fs.writeFileSync('database.json', JSON.stringify(dosya, null, 2))
+    }
+
+    /*
+    sıfırla => -
+    <DB>.sıfırla()
+    */
+
+    sıfırla(){
+        const dosya = JSON.parse(fs.readFileSync('database.json', 'utf8'))
+        return fs.writeFileSync('database.json', JSON.stringify({}, null, 2))
+    }
+
+}
+
+
+module.exports = new DB()
---
+++ test.js
@@ -0,0 +1,3 @@
+const db = require('./index') 
+db.yaz('31', '31')
+//db.yedekle('veri')
